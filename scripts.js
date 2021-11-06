7f645f613b02eb969fe3ed65e6b2201ce64f1eb6
kbaggott1 <kevin.baggott1@gmail.com>
2021-11-06 18:53:18 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
-function test() {
+let defaultButtonColor = "background-color: rgb(80, 79, 79);"
+let selectedButtonColor = "background-color: rgb(121, 121, 121);"
+
+function select(id) {
+    document.getElementById(id).style=selectedButtonColor;
+    if (id == "mbutton1") {
+        displayDate();
+    }
+    
+}
+
+function displayDate() {
     document.getElementById("demo").innerHTML = Date();
 }
