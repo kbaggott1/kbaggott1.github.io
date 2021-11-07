5c6ff99e5b0daa1fcab3901c30812fddc864ba2c
kbaggott1 <kevin.baggott1@gmail.com>
2021-11-07 13:45:44 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
 let defaultButtonColor = "background-color: rgb(80, 79, 79);"
 let selectedButtonColor = "background-color: rgb(121, 121, 121);"
 let MenuButtons = ["1YearP", "2YearP", "3YearP", "aboutMe"]
-function select(idSel) {
-    
+let ShowPages = ["1YearPage", "2YearPage", "3YearPage", "abMePage"]
+
+
+
+function select(idSel, pageToDisplay) {
+
     MenuButtons.forEach(element => {
         document.getElementById(element).style=defaultButtonColor; //sets all buttons to default color
     });
+    document.getElementById(idSel).style=selectedButtonColor; //sets selected button to highlighted color 
 
-    document.getElementById(idSel).style=selectedButtonColor; //sets selected button to highlighted color   
+    ShowPages.forEach(element => { 
+        document.getElementById(element).style.display = "none"; //Hides all content
+    });
+
+    document.getElementById(pageToDisplay).style.display = "block";
+    
 }
 
