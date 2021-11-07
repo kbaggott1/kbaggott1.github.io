ec5b9afb92f8041debbf566b22f4129b8b794d0c
kbaggott1 <kevin.baggott1@gmail.com>
2021-11-07 10:03:01 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 let defaultButtonColor = "background-color: rgb(80, 79, 79);"
 let selectedButtonColor = "background-color: rgb(121, 121, 121);"
-
-function select(id) {
-    document.getElementById(id).style=selectedButtonColor;
-    if (id == "mbutton1") {
-        displayDate();
-    }
+let MenuButtons = ["mbutton1", "mbutton2", "mbutton3"]
+function select(idSel) {
     
+    MenuButtons.forEach(element => {
+        document.getElementById(element).style=defaultButtonColor; //sets all buttons to default color
+    });
+
+    if (idSel == "mbutton1") {
+        displayDate();
+    } 
+    document.getElementById(idSel).style=selectedButtonColor; //sets selected button to highlighted color   
 }
 
 function displayDate() {
