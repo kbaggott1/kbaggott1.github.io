dcda6bfa8445627b86e89f623b7561c82fff0261
kbaggott1 <kevin.baggott1@gmail.com>
2021-11-07 21:24:57 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-let defaultButtonColor = "background-color: rgb(80, 79, 79);"
-let selectedButtonColor = "background-color: rgb(121, 121, 121);"
+let defaultButtonColor = "background-color: rgb(58, 56, 61);"
+let selectedButtonColor = "background-color: rgb(86, 86, 86);"
 let MenuButtons = ["1YearP", "2YearP", "3YearP", "aboutMe"]
 let ShowPages = ["1YearPage", "2YearPage", "3YearPage", "abMePage"]
 
